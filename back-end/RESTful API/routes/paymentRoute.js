4492ec6553e5193b294c7c8c44488365dfa76cfb
ntua-el21016 <el21016@mail.ntua.gr>
2025-02-10 23:10:07 +0200

    Update paymentRoute.js

--- back-end/RESTful API/routes/paymentRoute.js
+++ back-end/RESTful API/routes/paymentRoute.js
@@ -1,7 +1,8 @@
 const { paymentController } = require('../controllers/paymentsController');
+const {authenticateToken}=require('../utils/authentication_utils');
 const express = require('express');
 const router = express.Router();
 
-router.get('/api/getAmountsDue', paymentController);
+router.get('/api/getAmountsDue', authenticateToken,paymentController);
 
 module.exports = router;
