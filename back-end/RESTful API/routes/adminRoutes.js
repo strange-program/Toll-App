32f08c150d37243efc30467be2e571d104f62274
ntua-el21067 <el21067@mail.ntua.gr>
2025-02-10 20:01:18 +0200

    Add files via upload

--- back-end/RESTful API/routes/adminRoutes.js
+++ back-end/RESTful API/routes/adminRoutes.js
@@ -3,8 +3,6 @@ const multer = require('multer');
 const { upload } = require('../utils/upload');
 const router = express.Router();
 
-
-
 const { healthCheck } = require('../controllers/healthcheckController');
 router.get('/healthcheck', healthCheck);
 
@@ -16,5 +14,8 @@ router.get('/resetpasses', resetpasses);
 
 const { addPasses } = require('../controllers/addPassesController');
 router.post('/addpasses', upload.single('file'),addPasses);
+
+const { getUsers } = require('../controllers/getUsersController');
+router.get('/getUsers', getUsers);
 
 module.exports = router;
