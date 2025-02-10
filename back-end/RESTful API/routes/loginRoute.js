375fdd4b303e75be3dcb012c91279e0d9103177b
ntua-el21212 <el21212@mail.ntua.gr>
2025-02-10 23:07:59 +0200

    Update loginRoute.js
    - SECRET_KEY now obtained from the config.env file
    - added a try catch for an error 500

--- back-end/RESTful API/routes/loginRoute.js
+++ back-end/RESTful API/routes/loginRoute.js
@@ -7,23 +7,27 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = "your_secret_key"; // Αντικατέστησέ το με ένα πιο ασφαλές κλειδί
+const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key"; 
 
 router.post("/api/login", express.urlencoded({ extended: true }), async (req, res) => {
-    const { username, password } = req.body;
-    console.log("username",username);
-    console.log("password",password);
-    const user = await User.findOne({ username });
-    console.log(user);
-    if (!user) return res.status(401).json({ message: "User not found" });
+    try {
+        const { username, password } = req.body;
+        console.log("username",username);
+        console.log("password",password);
+        const user = await User.findOne({ username });
+        console.log(user);
+        if (!user) return res.status(401).json({ message: "User not found" });
   
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) return res.status(401).json({ message: "Invalid password" });
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) return res.status(401).json({ message: "Invalid password" });
   
-    const token = jwt.sign({ username: user.username , tollOpID:user.tollOpID }, SECRET_KEY, { expiresIn: "1h" });
+        const token = jwt.sign({ username: user.username , tollOpID:user.tollOpID }, SECRET_KEY, { expiresIn: "1h" });
   
-    res.json({ token, token });
+        res.status(200).json({ token, token });
+    }
+    catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
   });
 
-  
 module.exports = router;
