3a5bca57804bf6b8a108fdb91877eaa3658aaf27
ntua-el21016 <el21016@mail.ntua.gr>
2025-02-10 23:09:15 +0200

    Update registerRoutes.js

--- back-end/RESTful API/routes/registerRoutes.js
+++ back-end/RESTful API/routes/registerRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { registerAdmin } = require("../controllers/registerController");
+const {authenticateTokenAdmin}=require('../utils/authentication_admin_util');
 
 const router = express.Router();
 
-router.post("/api/admin/register", registerAdmin);
+router.post("/api/admin/register", authenticateTokenAdmin,registerAdmin);
 
 module.exports = router;
