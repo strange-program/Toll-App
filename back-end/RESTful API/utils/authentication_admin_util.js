66e076add3786e7a23e52595ec755ce03e34d077
ntua-el21016 <el21016@mail.ntua.gr>
2025-02-11 00:03:29 +0200

    Add files via upload

---
+++ back-end/RESTful API/utils/authentication_admin_util.js
@@ -0,0 +1,27 @@
+
+     const jwt = require("jsonwebtoken");
+     const {tokenBlacklist}=require("./authentication_utils.js");   
+      
+        
+        const authenticateTokenAdmin = (req, res, next) => {
+            const token = req.headers["x-observatory-auth"]; // Παίρνουμε το token από το request header
+        
+            if (!token) {
+                return res.status(401).json({ message: "Access denied. No token provided." });
+            }
+        
+            if (tokenBlacklist.has(token)) {
+                return res.status(403).json({ message: "Token is invalid (Logged out)." });
+            }
+        
+            try {
+                const decoded = jwt.verify(token, "your_secret_key"); // Επαλήθευση του token
+                if(decoded.username != "admin") return res.status(401).json({ message: "Access denied. Only admin is permitted" });
+                req.user = decoded; // Αποθηκεύουμε τα στοιχεία του χρήστη στο request
+                next(); // Συνεχίζουμε στο επόμενο middleware / route handler
+            } catch (err) {
+                return res.status(403).json({ message: "Invalid token." });
+            }
+        };
+        
+        module.exports = { authenticateTokenAdmin };
